8cc19e766d598585df4030724241d5cf1dc3073c
tguterres <tiagofguterres@gmail.com>
2018-12-08 12:27:06 +0000

    aula11 terminada

--- aulas/aula11.py
+++ aulas/aula11.py
@@ -1,1 +1,9 @@
-# Cores no Python - testing pull from master
+# Cores no Python
+# Style: 0 == none ; 1 == bold ; 4 == underline ; 7 == negative
+# Cor: 30-37
+# Background: 40-47
+import colorama
+colorama.init()
+print('\033[35mFor those about to rock.')
+
+ 
