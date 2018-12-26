454fe9a4369c1904e9ccbaf2dafef91f929e9ba0
tguterres <tiagofguterres@gmail.com>
2018-12-26 13:03:18 +0000

    jokenpo running

--- exercícios/exer45.py
+++ exercícios/exer45.py
@@ -1,30 +1,48 @@
 #Jokenpô
-import random
-papel = str('Papel').strip().lower()
-tesoura = str('Tesoura').strip().lower()
-pedra = str('Pedra').strip().lower()
-sorteio = [papel, tesoura, pedra]
-jokenpo = random.choice(sorteio)
-
-jkp = str(input('Escolha:\n'
-    '1 para papel\n'
-    '2 para tesoura\n'
-    '3 para pedra\n'
-    ''))
-
-if jkp == 1:
-    if jokenpo == papel == jkp:
+from random import randint
+from time import sleep
+jokenpo = ('Pedra', 'Papel', 'Tesoura')
+pc = randint(0 , 2)
+print('''Escolha entre:
+[0] Pedra
+[1] Papel
+[2] Tesoura''')
+player = int(input('Qual é a sua jogada? '))
+print('--' * 10)
+print('O computador escolheu {}.'.format(jokenpo[pc]))
+print('Você escolheu {}.'.format(jokenpo[player]))
+print('--' * 10)
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PÔ!')
+if pc == 0:
+    if player == 0:
         print('Empate.')
-    elif jokenpo == tesoura:
+    elif player == 1:
         print('Você ganhou!')
-    elif jokenpo == pedra:
+    elif player == 2:
         print('Você perdeu!')
-elif jkp == 2:
-    if jokenpo == tesoura == jkp:
+    else:
+        print('Jogada inválida.')
+elif pc == 1:
+    if player == 1:
         print('Empate.')
-    elif jokenpo == papel:
-        print('Você ganhou!')
-    elif jokenpo == pedra:
+    elif player == 0:
         print('Você perdeu!')
+    elif player == 2:
+        print('Você ganhou!')
+    else:
+        print('Jogada inválida.')
+elif pc == 2:
+    if player == 2:
+        print('Empate.')
+    elif player == 0:
+        print('Você ganhou!')
+    elif player == 1:
+        print('Você perdeu!')
+    else:
+        print('Jogada inválida.')
 
 
