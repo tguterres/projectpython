c7ba658d52ca7927a47258b650d1504ae3b060b6
tguterres <tiagofguterres@gmail.com>
2018-12-26 15:53:16 +0000

    limpando files

--- exercícios/pedrapapeltesoura.py
+++
@@ -1,9 +0,0 @@
-# Pedra Papel Tesoura
-import random
-lis1 = ['pedra', 'papel', 'tesoura']
-sort = random.choice(lis1)
-
-n = str(input('Escolha entre Padre, Papel ou Tesoura:')).strip().lower()
-if (n == sort):
-    print('Empate.')
-
