599abbb1cc68947ced57b36d6b83a82dd4902aed
tguterres <tiagofguterres@gmail.com>
2018-12-26 16:56:14 +0000

    limpando files

---
+++ aulas/aula13.py
@@ -0,0 +1,7 @@
+# Estruturas de Repetição - Laço de Repetição
+for c in range(1,5):
+    print('Hello')
+print('FIM')
+
+for c in range(1, 5, 2):
+    print('Hi')
---
+++ exercícios/exer46.py
@@ -0,0 +1,7 @@
+#Contagem regressiva
+from time import sleep
+for c in range(10, 0, -1,):
+    sleep(1)
+    print(c)
+sleep(1)
+print('Feliz 2019!')
