62c85782f5082c1845a143fa6e498a156047bb6a
tguterres <tiagofguterres@gmail.com>
2018-12-27 19:57:31 +0000

    exercicio 50, 51

--- exercícios/exer50.py
+++ exercícios/exer50.py
@@ -1,7 +1,8 @@
 # 6 números inteiros
 s = 0
 for c in range(0,6):
-    n = int(input('Digite um número inteiro: '))
-    if n % 2 == 0 and n != 0:
-        s += n
+   n = int(input('Digite um número inteiro: '))
+   if n % 2 == 0 and n != 0:
+       s += n
 print(s)
+
---
+++ exercícios/exer51.py
@@ -0,0 +1,7 @@
+#PA
+
+n = int(input('Digite o primeiro termo da PA: '))
+rz = int(input('Digite a razão da PA: '))
+for c in range(n, rz*10, rz):
+    print(c)
+
