4ff4a17231820b94d8fde58edd0c2251ca41acea
tguterres <tiagofguterres@gmail.com>
2018-12-27 15:48:16 +0000

    exercicio 50

---
+++ exercícios/exer49.py
@@ -0,0 +1,4 @@
+#Tabuada
+t = int(input('Quero ver a tabuada do: '))
+for t in range(t, t*10, t):
+    print(t)
---
+++ exercícios/exer50.py
@@ -0,0 +1,7 @@
+# 6 números inteiros
+s = 0
+for c in range(0,6):
+    n = int(input('Digite um número inteiro: '))
+    if n % 2 == 0 and n != 0:
+        s += n
+print(s)
