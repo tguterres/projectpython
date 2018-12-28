003f3564af32f8101c422d1cad043bc550f03a52
tguterres <tiagofguterres@gmail.com>
2018-12-28 14:06:59 +0000

    Primos

--- exercícios/exer52.py
+++ exercícios/exer52.py
@@ -1,3 +1,15 @@
 #Primos
+import colorama
 n = int(input('Número: '))
-if
+tot = 0
+for c in range(1, n+1):
+    if n % c == 0:
+        print('\33[33m', end='')
+        tot = tot+1
+    else:
+        print('\33[31m', end='')
+    print('{}'.format(c), end='')
+if tot == 2:
+    print('\nO número é primo.')
+else:
+    print('\nO número NÃO é primo.')
